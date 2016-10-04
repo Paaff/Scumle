3271dc1de58d0ef403633ab498046537fbf2210b
Mathias Mortensen <tympanix@gmail.com>
2016-10-04 11:56:34 +0200

    Revert "Added RibbonWindow"
    This reverts commit 6d75420afd889b70e975773b0546c6555fb1c814.

--- Scumle/Scumle/MainWindow.xaml.cs
+++ Scumle/Scumle/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,11 +18,11 @@ namespace Scumle
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : RibbonWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            
+            InitializeComponent();
         }
     }
 }
