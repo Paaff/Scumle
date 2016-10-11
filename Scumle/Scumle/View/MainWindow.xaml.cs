ccd0954db475a38d44bf5e06c39467ca5dc5eec1
Paaff <pafischer1992@gmail.com>
2016-10-11 10:46:22 +0200

    Created Ribbon Usercontrol

--- Scumle/Scumle/View/MainWindow.xaml.cs
+++ Scumle/Scumle/View/MainWindow.xaml.cs
@@ -6,15 +6,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Ribbon;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Scumle
+namespace Scumle.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
