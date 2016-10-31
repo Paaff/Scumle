ea0eb1b50fc8f6bd91a5cc6bb7b87e1fe02f2002
Mathias <tympanix@gmail.com>
2016-10-31 17:36:21 +0100

    Started on model for connectionpoints and lines

---
+++ Scumle/Scumle/ViewModel/LineViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Scumle.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scumle.ViewModel
+{
+    class LineViewModel : ViewModelBase<Line>
+    {
+        private ConnectionPointViewModel _from;
+        private ConnectionPointViewModel _to;
+
+        public LineViewModel(ConnectionPointViewModel from, ConnectionPointViewModel to) : base(new Line(from.Model, to.Model))
+        {
+            _from = from;
+            _to = to;
+        }
+
+        #region Properties
+        public ConnectionPointViewModel From
+        {
+            get { return _from; }
+            set
+            {
+                _from = value;
+                Model.From = value.Model;
+                OnPropertyChanged();
+            }
+        }
+
+        public ConnectionPointViewModel To
+        {
+            get { return _to; }
+            set
+            {
+                _to = value;
+                Model.To = value.Model;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
