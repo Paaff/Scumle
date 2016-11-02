f67123e8564c39a7a02b4972569022041a732837
Mathias <tympanix@gmail.com>
2016-11-02 21:25:53 +0100

    Added shape move and InotifyPropertyChanged

--- TestMVVM.Model/Shape.cs
+++ TestMVVM.Model/Shape.cs
@@ -1,20 +1,46 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace TestMVVM.Model
 {
-    public abstract class Shape
+    public abstract class Shape : ViewModelBase
     {
+        private double _x;
+        private double _y;
+
+        public ICommand MoveShapeCommand { get; }
+
         public Shape(double x, double y)
         {
             X = x;
             Y = y;
+            MoveShapeCommand = new RelayCommand<DragDeltaEventArgs>(MoveShape);
+
         }
 
-        public double X { get; set; }
-        public double Y { get; set; }
+        public void MoveShape(DragDeltaEventArgs e)
+        {
+            X += e.HorizontalChange;
+            Y += e.VerticalChange;
+        }
+
+        public double X
+        {
+          get { return _x; }
+          set { Set(ref _x, value); }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set { Set(ref _y, value); }
+        }
     }
 }
