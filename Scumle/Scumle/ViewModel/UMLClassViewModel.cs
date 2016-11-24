582e2bb0cfc6a52a75de978a505c9d6b27a01252
Paaff <pafischer1992@gmail.com>
2016-11-24 13:41:29 +0100

    UML Model and ViewModel updated to contain list of methods and fields.

--- Scumle/Scumle/ViewModel/UMLClassViewModel.cs
+++ Scumle/Scumle/ViewModel/UMLClassViewModel.cs
@@ -9,36 +9,61 @@ using System.Collections.ObjectModel;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using Scumle.Model.Shapes;
+using System.Windows;
 
 namespace Scumle.ViewModel
 {
     public class UMLClassViewModel : ShapeViewModel
     {
 
-        private ObservableCollection<string> _fields = new ObservableCollection<string>();
-        private ObservableCollection<string> _methods = new ObservableCollection<string>();
+        public ObservableCollection<string> fields;
+        public ObservableCollection<string> methods;
 
-        private ICommand addFieldCommand;
-        private ICommand addMethodCommand;
+        public ICommand addFieldCommand => new RelayCommand<string>((s) => addField(s));
+        public ICommand addMethodCommand => new RelayCommand<string>((s) => addMethod(s));
 
         #region Constructor
         // This Constructor should be taking an UMLClass and not a Shape yes?
-        public UMLClassViewModel(UMLClass shape) : base(shape)
+        public UMLClassViewModel(UMLClass uml) : base(uml)
         {
             Width = 300;
             Height = 150;
             ShapeColor = new SolidColorBrush(Color.FromRgb(232, 232, 232));
 
-      
+            fields = new ObservableCollection<string>(uml.umlFields);
+            methods = new ObservableCollection<string>(uml.umlMethods);
 
-        }
 
-        private void addField(string field)
-        {
-            _fields.Add(field);
         }
         #endregion
 
+        #region Properties
+
+        public ObservableCollection<string> UMLFields
+        {
+            get { return fields; }
+            set { }
+        }
+
+        public ObservableCollection<string> UMLMethods
+        {
+            get { return methods; }
+            set { }
+        }
+        #endregion
+
+        private void addField(string field)
+        {
+            //UMLFields.Add(field);
+            MessageBox.Show(field);
+        }
+        
+        private void addMethod(string method)
+        {
+            //  UMLMethods.Add(method);
+            MessageBox.Show(method);
+        }
+      
 
 
     }
